287ea0bd7fa77bdc12679601d0d947cc9799eeb6
Stefan <me@Stefan.com>
2019-01-26 11:52:56 +0200

    commiting files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Pop Stefan',
+		username: 'Stefan',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'John Doe',
+		username: 'John',
+		email: 'johndoe@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Mary Jack',
+		username: 'Mary',
+		email: 'maryjack@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Bee Pate',
+		username: 'Bee',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Kunal Aleksandra',
+		username: 'Kunal',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Anamaria Ana',
+		username: 'Anamaria',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Nazzareno Anton',
+		username: 'Nazzareno',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Gotthilf Biljana',
+		username: 'Gotthilf',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'Olek Zlatko',
+		username: 'Olek',
+		email: 'popstefan@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'Lumir Elise',
+		username: 'Lumir',
+		email: 'popstefan@gmail.com'
+	}
+]
